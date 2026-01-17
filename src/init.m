2e9af118d040a99ae31f4d3f0885e268f7ce1ef1
C-BC <c1505769513@gmail.com>
2026-01-17 13:22:49 -0800

    input Parser

--- src/init.m
+++ src/init.m
@@ -1,24 +1,46 @@
+function [position,velocity]=placeHolder111(tspan,dt,varargin)
 
 
+p=inputParser();
 
+% Required input
+p.addRequired('tspan',@(x) isnumeric(x))
+p.addRequired('dt', @(x) isnumeric(x));
+
+%optional input
+p.addParameter('massRange',[1,100], mRange, @(x) isnumeric(x) && isvector(x) );
+p.addParameter('dampingRange',[1,100], bRange, @(x) isnumeric(x) && isvector(x) );
+p.addParameter('stiffnessRange', [1,100],kRange, @(x) isnumeric(x) && isvector(x) );
+p.addParameter('positionRange',[1,100],positionRange, @(x) isnumeric(x) && isvector(x) )
+p.addParameter('velocityRange',[1,100],velocityRange, @(x) isnumeric(x) && isvector(x) )
+
+p.parse(tspan,dt,varargin{:});
+
+
+%open model
 mdl = "Mass_Spring_Damper_System";
 open_system(mdl)
 
-%Initial conditions
-v0 = 0; %velocity m/s
-x0 = 0; %position m
-m = 1; %mass kg
-b = 1; %Damping Ns/m
-k = 1; %stiffness N/m
+
+%initialize conditions
+v0 = rand()*(velocityRange(1)-velocityRange(0))+velocityRange(0); %velocity m/s
+x0 = rand()*(positionRange(1)-positionRange(0))+potitionRange(0); %position m
+m = rand()*(mRange(1)-mRange(0))+mRange(0); %mass kg
+b = rand()*(bRange(1)-bRange(0))+bRange(0); %Damping Ns/m
+k = rand()*(kRange(1)-kRange(0))+kRange(0); %stiffness N/m
 
 %Setting runtime
-tspan = 20; %duration (s)
-dt = 1/100; %step size (s)
+tspan = tspan; %duration (s)
+dt = dt; %step size (s)
 
 dt_data_target = 1/30;
 dt_data = round((dt_data_target/dt))*dt; %make sure dt_data is a multiple of dt_sim
 
+%load the model
 simIn = Simulink.SimulationInput(mdl);
+simIn = simIn.setModelParameter("StopTime",tspan);
+simIn = simIn.setModelVariable()
+
 simout = sim(simIn);
 v = simout.v.Data;
 x = simout.x.Data;
@@ -37,3 +59,7 @@ plot(t, v);
 xlabel('Time (s)');
 ylabel('Velocity (m/s)');
 title('Velocity vs Time');
+
+
+
+end
