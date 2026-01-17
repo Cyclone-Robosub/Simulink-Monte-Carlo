8ce85b1d122613e79c6d1afa5d6726bb9bb3d8ac
Jchill6 <jchil@ucdavis.edu>
2026-01-16 17:47:08 -0800

    made mass spring damper

---
+++ src/init.m
@@ -0,0 +1,39 @@
+
+
+
+mdl = "Mass_Spring_Damper_System";
+open_system(mdl)
+
+%Initial conditions
+v0 = 0; %velocity m/s
+x0 = 0; %position m
+m = 1; %mass kg
+b = 1; %Damping Ns/m
+k = 1; %stiffness N/m
+
+%Setting runtime
+tspan = 20; %duration (s)
+dt = 1/100; %step size (s)
+
+dt_data_target = 1/30;
+dt_data = round((dt_data_target/dt))*dt; %make sure dt_data is a multiple of dt_sim
+
+simIn = Simulink.SimulationInput(mdl);
+simout = sim(simIn);
+v = simout.v.Data;
+x = simout.x.Data;
+t = simout.v.Time;
+
+% Plot the results
+figure;
+subplot(2,1,1);
+plot(t, x);
+xlabel('Time (s)');
+ylabel('Position (m)');
+title('Position vs Time');
+
+subplot(2,1,2);
+plot(t, v);
+xlabel('Time (s)');
+ylabel('Velocity (m/s)');
+title('Velocity vs Time');
